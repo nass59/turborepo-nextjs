f5bf68f148e74362df2aa9ef2cbaa3049a721976
Nassim Afrete <nassim.afrete@gmail.com>
2022-09-23 17:18:25 +0200

    [NEXT] - ISR

---
+++ apps/web/pages/isr/blog.tsx
@@ -0,0 +1,43 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export default function Blog({ posts }: { posts: Post[] }) {
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  if (!res.ok) {
+    // If there is a server error, you might want to
+    // throw an error instead of returning so that the cache is not updated
+    // until the next successful request.
+    throw new Error(`Failed to fetch posts, received status ${res.status}`);
+  }
+
+  // If the request was successful, return the posts
+  // and revalidate every 10 seconds.
+  return {
+    props: {
+      posts,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 60, // In seconds
+  };
+};
