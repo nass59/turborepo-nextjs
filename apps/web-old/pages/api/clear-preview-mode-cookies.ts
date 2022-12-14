20796aad4ec7b1c1e1d7e467caf0e6e6ef495e21
Nassim Afrete <nassim.afrete@gmail.com>
2022-12-14 13:50:08 +0100

    [NEXT] remove old files

--- apps/web-old/pages/api/clear-preview-mode-cookies.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.clearPreviewData();
-  res.end("Cookies cleared");
-}
