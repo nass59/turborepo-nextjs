80f50f0e91d291f5a65389f918f10dde23b7db4a
Nassim Afrete <nassim.afrete@gmail.com>
2023-05-07 17:46:47 +0200

    chore: update dependencies to v13.1.4

--- apps/web/pages/api/og.tsx
+++ apps/web/pages/api/og.tsx
@@ -5,7 +5,7 @@ import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
 export const config = {
-  runtime: "experimental-edge",
+  runtime: "edge",
 };
 
 const interRegular = fetch(
