93eba21a8af9cd570008846dd7bbe89a81d23363
Nassim Afrete <nassim.afrete@gmail.com>
2023-05-19 12:58:44 +0200

    chore: update config

--- prettier.config.js
+++ prettier.config.js
@@ -18,7 +18,7 @@ module.exports = {
     "^@styles/(.*)$",
     "^[./]",
   ],
-  importOrderSeparation: false,
+  importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   importOrderBuiltinModulesToTop: true,
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
