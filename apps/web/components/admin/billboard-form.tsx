737f048f2ab6087c9a4010d5e24d4b3f2609ea95
Nassim Afrete <nassim.afrete@gmail.com>
2023-08-01 23:41:26 +0200

    feat(billboard): :sparkles: add billboard form

---
+++ apps/web/components/admin/billboard-form.tsx
@@ -0,0 +1,179 @@
+"use client"
+
+import { useState } from "react"
+import { useParams, useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { type BillboardModel } from "@/lib/database/models/Billboard"
+// import { useOrigin } from "@/hooks/use-origin"
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Heading,
+  Input,
+  Separator,
+  toast,
+} from "@shared/ui"
+// import { ApiAlert } from "@/components/admin/api-alert"
+import { AlertModal } from "@/components/admin/modals/alert-modal"
+import { Icons } from "@/components/icons"
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+})
+
+type BillboardFormValues = z.infer<typeof formSchema>
+
+interface BillboardFormProps {
+  initialData: BillboardModel | null
+}
+
+const CONFIG_LABELS = {
+  create: {
+    title: "Create billboard",
+    desscription: "Add a new billboard",
+    toastMessage: "Billboard created.",
+    action: "Create",
+  },
+  edit: {
+    title: "Edit billboard",
+    desscription: "Edit a billboard",
+    toastMessage: "Billboard updated.",
+    action: "Save changes",
+  },
+}
+
+export const BillboardForm: React.FC<BillboardFormProps> = ({
+  initialData,
+}) => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const params = useParams()
+  const router = useRouter()
+  // const origin = useOrigin()
+
+  const labels = initialData ? CONFIG_LABELS.edit : CONFIG_LABELS.create
+
+  const form = useForm<BillboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      label: "",
+      imageUrl: "",
+    },
+  })
+
+  const onSubmit = async (data: BillboardFormValues) => {
+    try {
+      setLoading(true)
+
+      await axios.patch(`/api/spaces/${params.spaceId}`, data)
+      router.refresh()
+
+      toast({
+        title: "Space updated.",
+      })
+    } catch (error) {
+      toast({
+        title: "Something went wrong.",
+        variant: "destructive",
+        description: "Your space was not updated. Please try again.",
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+
+      await axios.delete(`/api/spaces/${params.spaceId}`)
+      router.refresh()
+      router.push("/dashboard")
+
+      toast({
+        title: "Space deleted.",
+      })
+    } catch (error) {
+      toast({
+        title: "Something went wrong.",
+        variant: "destructive",
+        description: "Make sure you removed all categories first.",
+      })
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+
+      <div className="flex items-center justify-between">
+        <Heading title={labels.title} description={labels.desscription} />
+
+        {initialData && (
+          <Button
+            variant="destructive"
+            size="icon"
+            disabled={loading}
+            onClick={() => setOpen(true)}
+          >
+            <Icons.trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Billboard label"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {labels.action}
+          </Button>
+        </form>
+      </Form>
+
+      <Separator />
+    </>
+  )
+}
---
+++ apps/web/lib/database/models/Billboard.ts
@@ -0,0 +1,37 @@
+import { Schema, model, models, type Types } from "mongoose"
+
+export type BillboardModel = {
+  _id: Types.ObjectId
+  spaceId: string
+  label: string
+  imageUrl: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+const BillboardSchema = new Schema<BillboardModel>({
+  spaceId: {
+    type: String,
+    required: [true, "Please provide a spaceId for this Billboard"],
+  },
+  label: {
+    type: String,
+    required: [true, "Please provide a label for this Billboard"],
+    maxlength: [70, "Name cannot be more than 70 characters"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Please provide an imageUrl for this Billboard"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default models.Billboard ||
+  model<BillboardModel>("Billboard", BillboardSchema)
