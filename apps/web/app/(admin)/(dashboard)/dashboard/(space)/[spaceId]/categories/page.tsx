3bb94e4b39ef1b3e85b6eef754c6f64f5389ef43
Nassim Afrete <nassim.afrete@gmail.com>
2023-08-14 20:03:22 +0200

    feat(category): :sparkles: add category

---
+++ apps/web/app/(admin)/(dashboard)/dashboard/(space)/[spaceId]/categories/page.tsx
@@ -0,0 +1,32 @@
+import { format } from "date-fns"
+
+import { findAllCategoriesBySpaceId } from "@/lib/database/category"
+import { CategoryClient } from "@/components/admin/category-client"
+import { type CategoryColumn } from "@/components/admin/category-columns"
+
+interface PageProps {
+  params: {
+    spaceId: string
+  }
+}
+
+export default async function Page({ params }: PageProps) {
+  const categories = await findAllCategoriesBySpaceId(params.spaceId)
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item._id.toString(),
+    name: item.name,
+    billboardLabel: item.billboardId,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient
+          data={JSON.parse(JSON.stringify(formattedCategories))}
+        />
+      </div>
+    </div>
+  )
+}
