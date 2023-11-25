3103bdd47f060f888e0b76334a99187b96ffb3d3
Nassim Afrete <nassim.afrete@gmail.com>
2023-11-25 19:39:54 +0100

    feat(space): :sparkles: add charts on dashboard

--- apps/web/app/(admin)/(dashboard)/dashboard/(space)/[spaceId]/page.tsx
+++ apps/web/app/(admin)/(dashboard)/dashboard/(space)/[spaceId]/page.tsx
@@ -1,4 +1,19 @@
-import { findFirstBySpaceId } from "@/lib/database/space"
+import { countAllBillboardsBySpaceId } from "@/lib/database/billboard"
+import { countAllCategoriesBySpaceId } from "@/lib/database/category"
+import {
+  countAllItemsByMonthBySpaceId,
+  countAllItemsBySpaceId,
+} from "@/lib/database/items"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Heading,
+  Separator,
+} from "@shared/ui"
+import { Overview } from "@/components/dashboard/dashboard-overview"
+import { Icons } from "@/components/icons"
 
 interface DashboardSpaceProps {
   params: {
@@ -11,12 +26,65 @@ interface DashboardSpaceProps {
  * It then displays the name of the active space.
  */
 const Page = async ({ params }: DashboardSpaceProps) => {
-  const space = await findFirstBySpaceId(params.spaceId)
+  const { spaceId } = params
+
+  const totalBillboards = await countAllBillboardsBySpaceId(spaceId)
+  const totalCategories = await countAllCategoriesBySpaceId(spaceId)
+  const totalItems = await countAllItemsBySpaceId(spaceId)
+
+  const monthlyItems = await countAllItemsByMonthBySpaceId(spaceId)
 
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <div>
-        Active Space: <span className="font-semibold">{space?.name}</span>
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your space" />
+
+        <Separator />
+
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Billboards
+              </CardTitle>
+              <Icons.logo className="h-6 w-6" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalBillboards}</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Categories
+              </CardTitle>
+              <Icons.space className="h-6 w-6" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalCategories}</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Items</CardTitle>
+              <Icons.media className="h-6 w-6" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalItems}</div>
+            </CardContent>
+          </Card>
+
+          <Card className="col-span-3">
+            <CardHeader>
+              <CardTitle>Overview (Items by month)</CardTitle>
+            </CardHeader>
+            <CardContent className="pl-2">
+              <Overview data={monthlyItems} />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   )
