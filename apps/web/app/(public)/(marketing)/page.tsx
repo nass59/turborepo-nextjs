446f85069b5ddd183cdb1db3ae6c0f3ad76a5d0d
Nassim Afrete <nassim.afrete@gmail.com>
2023-12-12 16:05:17 +0100

    refactor(homepage): refactor layout landing page

--- apps/web/app/(public)/(marketing)/page.tsx
+++ apps/web/app/(public)/(marketing)/page.tsx
@@ -1,48 +1,17 @@
-import Link from "next/link"
-
-import { env } from "@/env.mjs"
 import { siteConfig } from "@/config/site"
 import { buttonVariants, cn } from "@shared/ui"
-import { FeatureItem } from "@/components/feature-item"
 import { Space3 } from "@/components/icons/space3"
-import { Auth } from "@/components/logos/auth"
-import { Github } from "@/components/logos/github"
-import { MongoDB } from "@/components/logos/mongodb"
-import { NextJS } from "@/components/logos/nextjs"
-import { ReactJS } from "@/components/logos/reactjs"
-import { Storybook } from "@/components/logos/storybook"
-import { TailwindCSS } from "@/components/logos/tailwindcss"
-
-type JsonResponse = {
-  stargazers_count: string
-}
-
-async function getGithubStars(): Promise<string | null> {
-  try {
-    const response = await fetch(
-      "https://api.github.com/repos/nass59/turborepo-nextjs",
-      {
-        headers: {
-          Accept: "application/vnd.github+json",
-          Authorization: `Bearer ${env.GITHUB_ACCESS_TOKEN}`,
-        },
-        next: {
-          revalidate: 3600,
-        },
-      }
-    )
-
-    if (!response?.ok) {
-      return null
-    }
-
-    const json = (await response.json()) as JsonResponse
-
-    return parseInt(json["stargazers_count"]).toLocaleString()
-  } catch (error) {
-    return null
-  }
-}
+import {
+  Auth,
+  Github,
+  MongoDB,
+  NextJS,
+  ReactJS,
+  Storybook,
+  TailwindCSS,
+} from "@/features/landing-page/assets/logos"
+import { FeatureItem } from "@/features/landing-page/ui/feature-item"
+import { getGithubStars } from "@/features/landing-page/utilities/github/stars"
 
 /**
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
@@ -103,7 +72,7 @@ export default async function Page() {
           <FeatureItem
             logo={<Auth />}
             title="Authentication"
-            description="Authentication using NextAuth.js and middlewares."
+            description="Authentication using Clerk and middlewares."
           />
           <FeatureItem
             logo={<TailwindCSS />}
@@ -133,7 +102,7 @@ export default async function Page() {
           </p>
 
           <div className="flex">
-            <Link
+            <a
               href={siteConfig.links.github}
               target="_blank"
               rel="noreferrer"
@@ -141,7 +110,7 @@ export default async function Page() {
             >
               <Github />
               Github
-            </Link>
+            </a>
             {stars && (
               <div className="flex items-center">
                 <div className="h-4 w-3 border-y-8 border-l-0 border-r-8 border-solid border-y-transparent border-r-slate-800"></div>
