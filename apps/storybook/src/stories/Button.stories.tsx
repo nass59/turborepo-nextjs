6df340d9aeab4dafe89cbc99da602e573fb7d826
Nassim Afrete <nassim.afrete@gmail.com>
2023-12-13 18:40:57 +0100

    refactor(blog): refactor avatar

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -2,10 +2,61 @@ import type { Meta, StoryObj } from "@storybook/react"
 
 import { Button, ButtonStyle } from "@shared/ui"
 
+type ButtonVariants = keyof typeof ButtonStyle.variants
+type ButtonSizes = keyof typeof ButtonStyle.sizes
+
+const variants = Object.keys(ButtonStyle.variants) as ButtonVariants[]
+const sizes = Object.keys(ButtonStyle.sizes) as ButtonSizes[]
+
 // @see https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
   title: "UI/Button",
   component: Button,
+  argTypes: {
+    variant: {
+      description: "Defines the variant of the button",
+      table: {
+        type: { summary: variants.join("|") },
+        defaultValue: { summary: "default" },
+      },
+      control: "select",
+      options: variants,
+    },
+    size: {
+      description: "Defines the size of the button",
+      table: {
+        type: { summary: sizes.join("|") },
+        defaultValue: { summary: "md" },
+      },
+      control: "select",
+      options: sizes,
+    },
+    children: {
+      description:
+        "Children components, usually text or an icon, that will be rendered inside the button",
+      table: {
+        defaultValue: { summary: null },
+      },
+    },
+    asChild: {
+      description:
+        "Radix will not render a default DOM element, instead cloning the part's child and passing it the props",
+      table: {
+        category: "Radix",
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+    },
+    className: {
+      description: "Override or extend the styles applied to the component",
+      control: "text",
+      table: {
+        category: "Override/extend",
+        type: { summary: "string" },
+        defaultValue: { summary: "" },
+      },
+    },
+  },
 } satisfies Meta<typeof Button>
 
 export default meta
@@ -98,14 +149,8 @@ export const Icon: Story = {
   },
 }
 
-type ButtonVariants = keyof typeof ButtonStyle.variants
-type ButtonSizes = keyof typeof ButtonStyle.sizes
-
 export const All: Story = {
   render: () => {
-    const variants = Object.keys(ButtonStyle.variants) as ButtonVariants[]
-    const sizes = Object.keys(ButtonStyle.sizes) as ButtonSizes[]
-
     return (
       <>
         {variants.map((variant) => (
