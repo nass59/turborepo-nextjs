aeacc40a419c2517cae7f134c5c4bb8ef73c3288
Nassim Afrete <nassim.afrete@gmail.com>
2023-12-19 19:05:36 +0100

    refactor(explorer): refactor explorer page

--- apps/web/features/explore/ui/billboard.tsx
+++ apps/web/features/explore/ui/billboard.tsx
@@ -8,19 +8,26 @@ type Props = {
 
 export const Billboard = ({ data }: Props) => {
   return (
-    <div className="overflow-hidden">
-      <div className="relative aspect-video overflow-hidden md:aspect-[2.4/1]">
-        <Image
-          src={data?.imageUrl}
-          alt={data?.label}
-          fill
-          className="z-0 object-cover object-center brightness-[0.6]"
-        />
-        <div className="flex h-full w-full items-center justify-center">
-          <div className="z-[1] max-w-xs font-heading text-4xl font-bold text-white sm:max-w-xl sm:text-5xl lg:text-6xl">
-            {data?.label}
+    <div className="relative block w-full overflow-hidden">
+      <div className="flex py-44">
+        <div className="container z-[1] flex items-center">
+          <div className="max-w-xs pl-2 text-white sm:max-w-2xl">
+            <h2 className="pb-7 pt-14 font-heading text-4xl font-bold sm:text-5xl lg:text-8xl">
+              {data?.label}
+            </h2>
           </div>
         </div>
+
+        <figure className="absolute left-0 top-0 h-full w-full object-cover">
+          <Image
+            src={data?.imageUrl}
+            alt={data?.label}
+            fill
+            className="z-0 object-cover object-center brightness-[0.6]"
+          />
+        </figure>
+
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900"></div>
       </div>
     </div>
   )
--- apps/web/features/explore/ui/list.tsx
+++ apps/web/features/explore/ui/list.tsx
@@ -1,7 +1,7 @@
 import { type ItemModel } from "@/lib/database/models/Item"
 import { parseData } from "@/lib/utils"
-import ExploreCard from "@/components/explore/explore-card"
 
+import { ExploreCard } from "./card"
 import { NoResults } from "./no-results"
 
 type Props = {
@@ -13,7 +13,7 @@ const Items = ({ items }: Pick<Props, "items">) => {
   if (items.length === 0) return <NoResults />
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {items.map((item) => (
         <ExploreCard key={item._id.toString()} data={parseData(item)} />
       ))}
@@ -23,8 +23,8 @@ const Items = ({ items }: Pick<Props, "items">) => {
 
 export const List = ({ title, items }: Props) => {
   return (
-    <div className="space-y-4">
-      <h3 className="text-3xl font-bold">{title}</h3>
+    <div className="space-y-8">
+      <h3 className="text-4xl font-bold">{title}</h3>
       <Items items={items} />
     </div>
   )
