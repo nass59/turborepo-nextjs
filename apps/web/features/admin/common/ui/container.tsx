eeac18c079dc65ea59acde11a4e11ffc0d413226
Nassim Afrete <nassim.afrete@gmail.com>
2024-01-28 18:31:05 +0100

    feat(e-learning): :sparkles: add e-learning dashboard

--- apps/web/features/admin/common/ui/container.tsx
+++ apps/web/features/admin/common/ui/container.tsx
@@ -2,7 +2,7 @@ import { type ComponentProps } from "react"
 
 export const Container = ({ children }: ComponentProps<"div">) => {
   return (
-    <div className="relative flex flex-col space-y-8 p-8 pl-64 pt-3">
+    <div className="relative flex flex-col space-y-8 p-8 pt-5 md:pl-64 md:pt-3">
       {children}
     </div>
   )
