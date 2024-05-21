9244ee219b68d9f029ddfa6cfd84e310e5f3fca0
Nassim Afrete <nassim.afrete@gmail.com>
2024-05-21 20:14:23 +0200

    chore(deps): update dependencies

--- apps/web/app/api/spaces/[spaceId]/items/[itemId]/route.ts
+++ apps/web/app/api/spaces/[spaceId]/items/[itemId]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { auth } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
 
 import { deleteOneItem, findOneItem, updateOneItem } from "@/lib/database/items"
 import { findOneSpace } from "@/lib/database/space"
--- apps/web/app/api/spaces/route.ts
+++ apps/web/app/api/spaces/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { auth } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
 
 import { createSpace } from "@/lib/database/space"
 
