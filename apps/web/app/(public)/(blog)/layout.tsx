433ab6144e78d9810ab162cb4a9baba1d90199ea
Nassim Afrete <nassim.afrete@gmail.com>
2024-06-22 22:52:26 +0200

    chore(deps): update dependencies

--- apps/web/app/(public)/(blog)/layout.tsx
+++ apps/web/app/(public)/(blog)/layout.tsx
@@ -2,9 +2,6 @@ import { type LayoutProps } from "@/types/common"
 import { marketingConfig } from "@/config/marketing"
 import { PageWrapper } from "@/features/public/common/ui/page-wrapper"
 
-/**
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/layout
- */
 export default function Layout({ children }: LayoutProps) {
   return (
     <PageWrapper mainNavItems={marketingConfig.mainNav}>{children}</PageWrapper>
