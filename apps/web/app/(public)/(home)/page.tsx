2809ba2a6e5565b97a2a79227df2f6de7975a635
Nassim Afrete <nassim.afrete@gmail.com>
2025-08-28 21:31:57 +0200

    feat: add AnimatedGridPattern component and integrate it into the home page layout

--- apps/web/app/(public)/(home)/page.tsx
+++ apps/web/app/(public)/(home)/page.tsx
@@ -1,4 +1,6 @@
+import { AnimatedGridPattern } from '@workspace/design-system/components/magicui/animated-grid-pattern';
 import { Separator } from '@workspace/design-system/components/ui/separator';
+import { cn } from '@workspace/design-system/lib/utils';
 import { Block } from '@/app/modules/home/ui/home-block';
 import { BlockSeparator } from '@/app/modules/home/ui/home-block-separator';
 import { Heading } from '@/app/modules/home/ui/home-heading';
@@ -12,13 +14,22 @@ export default function Page() {
     <div className="relative flex min-h-screen flex-col bg-[#030303]">
       <div className="items-start md:grid md:grid-cols-2">
         {/* Left side */}
-        <Block>
+        <Block className="overflow-hidden">
           <div className="mx-auto flex h-full max-w-2xl flex-col gap-2 md:justify-center">
             <Heading />
             <Separator className="mt-5 h-px text-gray-300" />
             <StackButtons />
           </div>
           <BlockSeparator />
+          <AnimatedGridPattern
+            className={cn(
+              '[mask-image:radial-gradient(500px_circle_at_center,orange,transparent)]',
+              'inset-x-0 inset-y-0 hidden h-[200%] skew-y-12 md:block'
+            )}
+            duration={3}
+            maxOpacity={0.1}
+            repeatDelay={1}
+          />
         </Block>
 
         {/* Right side */}
