47c3fb6270b5777fb7241b75a46a46760b59c5eb
Nassim Afrete <nassim.afrete@gmail.com>
2025-08-30 19:40:18 +0200

    feat: rename components for consistency and clarity; update imports in the home page

--- apps/web/app/(public)/(home)/page.tsx
+++ apps/web/app/(public)/(home)/page.tsx
@@ -1,10 +1,10 @@
 import { AnimatedGridPattern } from '@workspace/design-system/components/magicui/animated-grid-pattern';
 import { Separator } from '@workspace/design-system/components/ui/separator';
 import { cn } from '@workspace/design-system/lib/utils';
-import { Block } from '@/modules/home/ui/home-block';
-import { BlockSeparator } from '@/modules/home/ui/home-block-separator';
-import { Heading } from '@/modules/home/ui/home-heading';
-import { StackButtons } from '@/modules/home/ui/home-stack-buttons';
+import { HomeBlock } from '@/modules/home/ui/home-block';
+import { HomeBlockSeparator } from '@/modules/home/ui/home-block-separator';
+import { HomeHeading } from '@/modules/home/ui/home-heading';
+import { HomeStackButtons } from '@/modules/home/ui/home-stack-buttons';
 
 /**
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
@@ -14,13 +14,13 @@ export default function Page() {
     <div className="relative flex min-h-screen flex-col bg-[#030303]">
       <div className="items-start md:grid md:grid-cols-2">
         {/* Left side */}
-        <Block className="overflow-hidden">
+        <HomeBlock className="overflow-hidden">
           <div className="mx-auto flex h-full max-w-2xl flex-col gap-2 md:justify-center">
-            <Heading />
+            <HomeHeading />
             <Separator className="mt-5 h-px text-gray-300" />
-            <StackButtons />
+            <HomeStackButtons />
           </div>
-          <BlockSeparator />
+          <HomeBlockSeparator />
           <AnimatedGridPattern
             className={cn(
               '[mask-image:radial-gradient(500px_circle_at_center,orange,transparent)]',
@@ -30,16 +30,16 @@ export default function Page() {
             maxOpacity={0.1}
             repeatDelay={1}
           />
-        </Block>
+        </HomeBlock>
 
         {/* Right side */}
-        <Block>
+        <HomeBlock>
           <div className="mx-auto flex h-full max-w-2xl flex-col gap-2 md:justify-center">
             <h3 className="bg-clip-text font-bold text-slate-200 text-xl tracking-tighter">
               In construction
             </h3>
           </div>
-        </Block>
+        </HomeBlock>
       </div>
     </div>
   );
