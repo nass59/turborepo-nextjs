b93abc76d7eb82a377f9b0e9313a6857d94bec4f
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-15 18:06:24 +0200

    feat(database): init prisma (sql) database

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -25,6 +25,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
     ],
   },
   experimental: {
