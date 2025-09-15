5a2af7b613187cb48d064c0b6e04b32ac33cf0f4
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-15 16:29:36 +0200

    refactor: remove blog-related components and posts from the application

--- apps/web/app/(public)/(blog)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PageWrapper } from '@/features/public/common/ui/page-wrapper';
-import type { LayoutProps } from '@/types/common';
-
-/**
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/layout
- */
-export default function Layout({ children }: LayoutProps) {
-  return <PageWrapper>{children}</PageWrapper>;
-}
