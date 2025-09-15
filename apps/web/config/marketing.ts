64b2767f9c72c106ae599b908e0c4cc4bd875276
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-15 16:29:36 +0200

    refactor: remove blog-related components and posts from the application

--- apps/web/config/marketing.ts
+++ apps/web/config/marketing.ts
@@ -3,16 +3,6 @@ import type { MarketingConfig } from 'types';
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: 'Blog',
-      href: '/blog',
-      disabled: false,
-    },
-    {
-      title: 'Documentation',
-      href: '/docs',
-      disabled: false,
-    },
-    {
       title: 'Explore',
       href: '/explore',
       disabled: false,
