d87ed1475494374457795b1e82a953f9bf3da192
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-16 20:15:19 +0200

    Refactor code structure for improved readability and maintainability

--- apps/web/trpc/routers/_app.ts
+++ apps/web/trpc/routers/_app.ts
@@ -1,13 +1,20 @@
 import { z } from 'zod';
+import { inngest } from '@/inngest/client';
 import { baseProcedure, createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(z.object({ text: z.string() }))
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: 'test/hello.world',
+        data: { email: input.text },
+      });
+
+      return { success: true };
+    }),
   createAI: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
+    .input(z.object({ text: z.string() }))
     .query((opts) => {
       return {
         greeting: `hello ${opts.input.text}`,
