dce7132d4b7d983159800a8c226f24f3b1611d91
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-22 16:31:58 +0200

    feat(messages): implement message router with create and getMany procedures

---
+++ apps/web/prisma/migrations/20250922141158_sandbox/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Fragment` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Fragment" DROP COLUMN "name";
--- apps/web/trpc/routers/_app.ts
+++ apps/web/trpc/routers/_app.ts
@@ -1,25 +1,8 @@
-import { z } from 'zod';
-import { inngest } from '@/inngest/client';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { messageRouter } from '@/modules/messages/server/procedures';
+import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
-  invoke: baseProcedure
-    .input(z.object({ value: z.string() }))
-    .mutation(async ({ input }) => {
-      await inngest.send({
-        name: 'test/hello.world',
-        data: { value: input.value },
-      });
-
-      return { success: true };
-    }),
-  createAI: baseProcedure
-    .input(z.object({ text: z.string() }))
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  messages: messageRouter,
 });
 
 // export type definition of API
