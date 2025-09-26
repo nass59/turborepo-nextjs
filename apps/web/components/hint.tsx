77e9c7bddb7ec0edb2962073ed1282fb9165bddf
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-26 18:41:43 +0200

    feat: add Hint component and integrate into FragmentWeb; update ProjectView to display FragmentWeb conditionally

---
+++ apps/web/components/hint.tsx
@@ -0,0 +1,31 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@workspace/design-system/components/ui/tooltip';
+
+type Props = {
+  children: React.ReactNode;
+  text: string;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  align?: 'start' | 'center' | 'end';
+};
+
+export const Hint = ({
+  children,
+  text,
+  side = 'top',
+  align = 'center',
+}: Props) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent align={align} side={side}>
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
