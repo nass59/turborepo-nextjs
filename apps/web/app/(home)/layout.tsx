d33d8e43b421d4eba71a4e6f64cf0c3dc5539b88
Nassim Afrete <nassim.afrete@gmail.com>
2025-09-28 22:10:30 +0200

    fix: replace <main> with <div> in HomeLayout component for consistency

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -2,11 +2,11 @@ import type { LayoutProps } from '@/types/common';
 
 export default function HomeLayout({ children }: LayoutProps) {
   return (
-    <main className="flex max-h-screen min-h-screen flex-col">
+    <div className="flex max-h-screen min-h-screen flex-col">
       <div className="flex flex-1 flex-col px-4 pb-4">
         <div className="-z-10 absolute inset-0 h-full w-full bg-[radial-gradient(#dadde2_1px,transparent_1px)] bg-background [background-size:16px_16px] dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)]" />
         {children}
       </div>
-    </main>
+    </div>
   );
 }
